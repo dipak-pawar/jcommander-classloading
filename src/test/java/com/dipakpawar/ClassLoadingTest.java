ee7b4be896a3fd17b08270f1f53914488eb39ee9
Dipak Pawar <dpawar@redhat.com>
2018-02-26 21:14:18 +0530

    Adds reproducer for jcommander classloading

--- src/test/java/com/dipakpawar/ClassLoadingTest.java
+++ src/test/java/com/dipakpawar/ClassLoadingTest.java
@@ -1,26 +1,28 @@
 package com.dipakpawar;
 
+import com.beust.jcommander.ParameterException;
 import org.junit.Test;
 
 import java.net.URL;
+import org.junit.runners.JUnit4;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClassLoadingTest {
 
     @Test
-    public void should_load_jcommander() {
+    public void should_load_ParameterException_class() throws ClassNotFoundException {
         // when
-        URL resource = this.getClass().getClassLoader().getResource("com/beust/jcommander");
+        Class<?> resource = this.getClass().getClassLoader().loadClass(ParameterException.class.getName());
 
         // then
         assertThat(resource).isNotNull();
     }
 
     @Test
-    public void should_load_junit() {
+    public void should_load_JUnit4_class() throws ClassNotFoundException {
         // when
-        URL resource = this.getClass().getClassLoader().getResource("junit");
+        Class<?> resource = this.getClass().getClassLoader().loadClass(JUnit4.class.getName());
 
         // then
         assertThat(resource).isNotNull();
